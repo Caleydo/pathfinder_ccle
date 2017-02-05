e580f2412c9ad1f579ccbb88d9ab7113c7739ce5
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-02-05 12:00:32 +0100

    convert to typescript

--- src/index.ts
+++ src/index.ts
@@ -2,106 +2,104 @@
  * Created by Samuel Gratzl on 08.06.2015.
  */
 
-define(['exports', 'phovea_core/src/ajax'], function (exports, ajax) {
-  exports.list = function () {
-    return ajax.getAPIJSON('/ccle');
-  };
-  exports.data = function (dataset, row_ids, col_ids, method) {
-    row_ids = row_ids || [];
-    col_ids = col_ids || [];
-    method = method || 'data';
-    var param = {};
-    if (row_ids.length > 0) {
-      param['rows'] = row_ids;
-    }
-    if (col_ids.length > 0) {
-      param['cols'] = col_ids;
-    }
-    return ajax.getAPIJSON('/ccle/' + dataset + '/'+method, param);
-  };
-  exports.stats = function(dataset, row_ids, col_ids) {
-    return exports.data(dataset, row_ids, col_ids, 'stats');
-  };
-  exports.rows = function (dataset, row_ids) {
-    row_ids = row_ids || [];
-    var param = {};
-    if (row_ids.length > 0) {
-      param['rows'] = row_ids;
-    }
-    return ajax.getAPIJSON('/ccle/' + dataset + '/rows', param);
-  };
-  exports.cols = function (dataset, col_ids) {
-    col_ids = col_ids || [];
-    var param = {};
-    if (col_ids.length > 0) {
-      param['cols'] = col_ids;
-    }
-    return ajax.getAPIJSON('/ccle/' + dataset + '/cols', param);
-  };
-  exports.group = function (dataset, group) {
-    if (typeof group !== 'undefined') {
-      return ajax.getAPIJSON('/ccle/' + dataset + '/group/'+group);
-    } else {
-      return ajax.getAPIJSON('/ccle/' + dataset + '/group');
-    }
-  };
+import {getAPIJSON} from 'phovea_core/src/ajax';
+import Timer = NodeJS.Timer;
 
 
-  function boxplotImpl(genes, summaryOfGroups) {
-    genes = genes || null;
-    summaryOfGroups = summaryOfGroups || null;
-    var param = {};
-    if (genes) {
-      param['g'] = genes.join(',');
-    }
-    if (summaryOfGroups) {
-      param['groups'] = summaryOfGroups
-    }
-    return ajax.getAPIJSON('/ccle/boxplot', param);
+export function list() {
+  return getAPIJSON('/ccle');
+}
+
+export function data(dataset: string, rowIds: string[] = [], colIds: string[] = [], method: string = 'data') {
+  const param: any = {};
+  if (rowIds.length > 0) {
+    param.rows = rowIds;
   }
-
-  var cache = {};
-
-  function resolveImpl(key, summaryOfGroups) {
-    var t = cache[key];
-    var promise = boxplotImpl(t.to_be_queried.map(function(d) { return d.gene; }), summaryOfGroups);
-    var sent = t.to_be_queried;
-    sent.forEach(function(g) {
-      t.cache[g.gene] = promise;
-    });
-    promise.then(function(output) {
-      sent.forEach(function(g) {
-        g.callback(output[g.gene]);
-      });
-    });
-    t.to_be_queried = [];
+  if (colIds.length > 0) {
+    param.cols = colIds;
   }
-  exports.boxplot_of = function(gene, callback, summaryOfGroups) {
-    var key = summaryOfGroups ? summaryOfGroups.join('_') : '_individual';
-    var t;
-    if (key in cache) {
-      t = cache[key]
-    } else {
-      t = { to_be_queried : [], cache : {}, wait_timer : -1};
-      cache[key] = t
+  return getAPIJSON(`/ccle/${dataset}/${method}`, param);
+}
+
+export function stats(dataset: string, rowIds: string[] = [], colIds: string[] = []) {
+  return data(dataset, rowIds, colIds, 'stats');
+}
+export function rows(dataset: string, rowIds: string[] = []) {
+  const param: any = {};
+  if (rowIds.length > 0) {
+    param.rows = rowIds;
+  }
+  return getAPIJSON(`/ccle/${dataset}/rows`, param);
+}
+export function cols(dataset: string, colIds: string[] = []) {
+  const param: any = {};
+  if (colIds.length > 0) {
+    param.cols = colIds;
+  }
+  return getAPIJSON(`/ccle/${dataset}/cols`, param);
+}
+export function group(dataset: string, group?: string) {
+  if (typeof group !== 'undefined') {
+    return getAPIJSON(`/ccle/${dataset}/group/${group}`);
+  } else {
+    return getAPIJSON(`/ccle/${dataset}/group`);
+  }
+}
+
+
+function boxplotImpl(genes?: string[], summaryOfGroups?: string) {
+  const param: any = {};
+  if (genes) {
+    param.g = genes.join(',');
+  }
+  if (summaryOfGroups) {
+    param.groups = summaryOfGroups;
+  }
+  return getAPIJSON('/ccle/boxplot', param);
+}
+
+interface IQueried {
+  gene: string;
+  callback(r: any): void;
+}
+
+const cache = new Map<string, {toBeQueried: IQueried[], cache: Map<string, Promise<any>>, waitTimer: number}>();
+
+async function resolveImpl(key: string, summaryOfGroups?: string[]) {
+  const t = cache.get(key);
+  const promise = boxplotImpl(t.toBeQueried.map((d) => d.gene, summaryOfGroups));
+  const sent = t.toBeQueried;
+  sent.forEach((g) => {
+    t.cache.set(g.gene, promise);
+  });
+  t.toBeQueried.splice(0, t.toBeQueried.length);
+
+  const output: any = await promise;
+  sent.forEach((g) => {
+    g.callback(output[g.gene]);
+  });
+}
+
+export function boxplot_of(gene: string, callback: (r: any) => void, summaryOfGroups?: string[]) {
+  const key = summaryOfGroups ? summaryOfGroups.join('_') : '_individual';
+  if (!cache.has(key)) {
+    cache.set(key, {toBeQueried: [], cache: new Map<string, Promise<any>>(), waitTimer: -1});
+  }
+  const t = cache.get(key);
+  if (t.cache.has(gene)) {
+    t.cache.get(gene).then((output) => callback(output[gene]));
+  } else {
+    t.toBeQueried.push({gene, callback});
+    if (t.waitTimer > 0) {
+      clearTimeout(t.waitTimer);
+      t.waitTimer = -1;
     }
-    if (gene in t.cache) {
-      t.cache[gene].then(function(output) {
-        callback(output[gene]);
-      });
+    if (t.toBeQueried.length > 10) {
+      resolveImpl(key, summaryOfGroups);
     } else {
-      t.to_be_queried.push({ gene: gene, callback: callback});
-      if (t.wait_timer > 0) {
-        clearTimeout(t.wait_timer);
-        t.wait_timer = -1;
-      }
-      if (t.to_be_queried.length > 10) {
-        resolveImpl(key, summaryOfGroups)
-      } else {
-        t.wait_timer = setTimeout(function() {
-          resolveImpl(key, summaryOfGroups)
-        }, 200); //wait and collect 200 milliseconds
-      }
+      t.waitTimer = <any>setTimeout(() => {
+        resolveImpl(key, summaryOfGroups);
+      }, 200); //wait and collect 200 milliseconds
     }
-  };
-});
+  }
+}
